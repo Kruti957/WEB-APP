b2d92d8b365923030f8e657cf79df1ecfdae97b1
Kruti957 <100508828+Kruti957@users.noreply.github.com>
2022-05-26 14:28:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload(){
+
+}
+
+function setrup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide;
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+
+}
